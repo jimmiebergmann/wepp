543468ce641d0eaef1b6fd14c0057ccf47c71f4a
Jimmie Bergmann <jimmiebergmann@gmail.com>
2019-02-16 22:13:00 +0100

    Fixing gtest files.

--- test/test.cpp
+++ test/test.cpp
@@ -1,8 +1,9 @@
 #include <iostream>
 #include "wepp/uri.hpp"
+#include "gtest/gtest.h"
 
 using namespace Wepp;
-
+/*
 int main()
 {
     Uri uri("http://api.test.com/comment?id=4");
@@ -10,4 +11,16 @@ int main()
     std::cout << "Running test." << std::endl;
 
     return 0;
+}
+*/
+
+TEST(Property, Value)
+{
+    EXPECT_TRUE(true);
+}
+
+int main(int argc, char ** argv)
+{
+    testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
 }
