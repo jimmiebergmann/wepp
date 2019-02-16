8322ea46d4c495fd5cc5c0b7d9b3ff64d73e4d4a
Jimmie Bergmann <jimmiebergmann@gmail.com>
2019-02-16 21:24:09 +0100

    Added gtest submodule and added test project.

--- test/test.cpp
+++ test/test.cpp
@@ -1,7 +1,12 @@
 #include <iostream>
+#include "wepp/uri.hpp"
+
+using namespace Wepp;
 
 int main()
 {
+    Uri uri("http://api.test.com/comment?id=4");
+
     std::cout << "Running test." << std::endl;
 
     return 0;
