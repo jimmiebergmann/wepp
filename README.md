a1eea279a713d70c36deb2c6e5fe22894afac635
Jimmie Bergmann <jimmiebergmann@gmail.com>
2019-02-16 20:28:23 +0100

    Building project structures.

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # wepp
+[![Build status](https://ci.appveyor.com/api/projects/status/ku6wlu3qn3rcplug/branch/master?svg=true)](https://ci.appveyor.com/project/jimmiebergmann/wepp/branch/master)  [![License: MIT](https://img.shields.io/badge/License-MIT-brightgreen.svg)](https://opensource.org/licenses/MIT)
+
 A web microframework written in C++, providing a clean API for running REST servers/clients.
+
+## Status
+This library is currently in startup phase and not yet providing any useful functionality.
+Follow for more updates.
+
+## Dependencies
+* C++11
+* googletest (git submodule)
+
+## Example
+Not yet.
+
---
+++ include/uri.hpp
@@ -0,0 +1,116 @@
+/*
+* MIT License
+*
+* Copyright (c) 2019 Jimmie Bergmann
+*
+* Permission is hereby granted, free of charge, to any person obtaining a copy
+* of this software and associated documentation files(the "Software"), to deal
+* in the Software without restriction, including without limitation the rights
+* to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+* copies of the Software, and to permit persons to whom the Software is
+* furnished to do so, subject to the following conditions :
+*
+* The above copyright notice and this permission notice shall be included in all
+* copies or substantial portions of the Software.
+*
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.IN NO EVENT SHALL THE
+* AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+* OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+* SOFTWARE.
+*
+*/
+
+#ifndef WEPP_URI_HPP
+#define WEPP_URI_HPP
+
+#include "build.hpp"
+#include <string>
+
+
+
+/*
+* Wepp namespace.
+*
+*/
+namespace Wepp
+{
+
+    /*
+    * Uniform Resource Identifier structure and parser.
+    *
+    * @see https://en.wikipedia.org/wiki/Uniform_Resource_Identifier
+    *
+    */
+    class WEPP_API Uri
+    {
+
+    public:
+
+        /**
+        * Constuctor.
+        * Initializing empty URI structure.
+        *
+        */
+        Uri();
+
+        /**
+        * Constuctor.
+        * Initializing by parsing input data.
+        *
+        * @param uri[in] - Input uri to parse.
+        * @see parse
+        *
+        */
+        Uri(const char * uri);
+
+        /**
+        * Constuctor.
+        * Initializing by parsing input data.
+        *
+        * @param uri[in] - Input uri to parse.
+        * @see parse
+        *
+        */
+        Uri(const std::string & uri);
+
+        /**
+        * Parsing input data and populating URI structure.
+        *
+        * @param uri[in] - data to parse.
+        * @see parse
+        *
+        */
+        bool parse(const char * parse);
+
+        /**
+        * Parsing input data and populating URI structure.
+        *
+        * @param uri[in] - data to parse.
+        * @see parse
+        *
+        */
+        bool parse(const std::string & parse);
+
+        /**
+        * Validates and updates Uri members if needed.
+        * For example, schema is changed to "http" if schema == "http:".
+        *
+        * @return true if validation succeeded, else false.
+        *
+        */
+        bool validate();
+
+        std::string schema;    /**< Schema name. */
+        std::string authority; /**< Authority string. */
+        std::string path;      /**< Resource path. */
+        std::string query;     /**< Query string. */
+        std::string fragment;  /**< Fragment flag. */
+
+    };
+
+}
+
+#endif
